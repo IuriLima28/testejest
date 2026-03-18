142d60edb86570b3ecbedc3484fa4c18a13776e1
IURI D'OLIVEIRA DE LIMA <iuri_d_lima@estudante.sesisenai.org.br>
2026-03-18 14:54:22 -0300

    AAAAAAA

--- exercicio4/exercicio-04.js
+++ exercicio4/exercicio-04.js
@@ -1,14 +1,22 @@
-function calcularVendaComFrete() {
-  valorProdutos = Number(document.getElementById("preco").value)
-  let frete = 20;
+if (typeof window !== "undefined") {
+  window.calcularVendaComFrete = calcularVendaComFrete;
+}
 
+// exercicio-04.js
+function calcularVendaComFrete(valorProdutos) {
+  let frete = 20;
   if (valorProdutos > 200) {
-    frete = 0;
+      frete = 0;
   }
 
   const valorTotal = valorProdutos + frete;
 
-  alert("Valor produto: " + valorProdutos)
-  alert("Frete: 20")
-  alert("Valor total:" + valorTotal)
-};
+  return {
+      valorProdutos,
+      frete,
+      valorTotal
+  };
+}
+
+// Exporta a função corretamente
+module.exports = calcularVendaComFrete;
